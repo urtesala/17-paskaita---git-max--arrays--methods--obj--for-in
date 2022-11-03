598cbd3511a4d1772466f8fe9a769e1d24a77949
urtesala <urtesaladziute@gmail.com>
2022-11-03 09:26:10 +0300

    init

---
+++ js/main.js
@@ -0,0 +1,18 @@
+"use strict";
+console.log("main.js");
+
+const numbers = [1, 2, 3, 5.4, 8, -5.5, -6, -16, -5, -9, 4.2, 5, 12, -6, 45];
+
+//? 1. atrinkti skaicius didesnius uz 3 => filter
+
+//? 2. grazinti nauja masyva su padvigubintom reiksmem? => map
+
+//? 3. gauti neigiamu reiksmiu bendra suma => filter => reduce
+
+//? 4. suzinoti ar yra sk 5 musu numbers masyve? => includes
+
+//? 5. konsoleje atspausdinti visas reiksmes su indexais => forEach
+
+//? 6. every
+
+//? 7. some
